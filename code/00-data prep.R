c81bdd43ab456eb36dd158b8ab22b84a5d1c697a
Avery Hill <aph82@stanford.edu>
2024-07-24 18:32:46 -0700

    adding pollution, traffic, impervious surfaces

--- code/00-data prep.R
+++ code/00-data prep.R
@@ -1,4 +1,6 @@
 
+terrestrial.sf <- st_read("~/Data/Boundaries/Political/CLN2.0/CLN_Database_2_0_1/data/Base/vector/terrestrial.shp")
+
 
 #  ---------------------------------------
 # Species occurrence data ---------------------------------------
@@ -14,20 +16,20 @@ library(RPostgreSQL)
 # library(starsExtra)
 
 # sf_use_s2(T)
-counties.sf <- st_read("~/Data/Boundaries/Political/CA_Counties/CA_Counties_TIGER2016.shp") %>% 
+counties.sf <- st_read("~/Data/Boundaries/Political/CA_Counties/CA_Counties_TIGER2016.shp") |> 
   filter(NAME %in% c("Solano", "Contra Costa", "Alameda", "Santa Clara", "San Mateo",
-                     "San Francisco", "Marin", "Sonoma", "Napa", "Santa Cruz")) %>% 
+                     "San Francisco", "Marin", "Sonoma", "Napa", "Santa Cruz")) |> 
   st_transform(crs = 4326)
 target_counties.sf <- 
-  counties.sf %>% 
+  counties.sf |> 
   st_union()  # Convert to WGS 84
 
 # # Simplify input polygons and add some buffer zone to it
 target_simple.sf <- 
-  target_counties.sf %>% 
-  st_union(is_coverage = T) %>% 
-  st_buffer(4000) %>% # 1km buffer
-  st_simplify(dTolerance = 4000) %>% # reduce 
+  target_counties.sf |> 
+  st_union(is_coverage = T) |> 
+  st_buffer(4000) |> # 1km buffer
+  st_simplify(dTolerance = 4000) |> # reduce 
   st_cast("POLYGON")
 
 # write_sf(target_simple.sf[1], "../data/study_area.gpkg") # just write mainland
@@ -35,8 +37,8 @@ target_simple.sf <-
 
 # Convert to a well known text format of the polygon and check validity
 target.ewkt <- 
-  target_simple.sf %>% 
-  st_union() %>% 
+  target_simple.sf |> 
+  st_union() |> 
   st_as_text(EWKT = T)
 
 # Now pull from local GBIF database 
@@ -66,3 +68,78 @@ write_sf(target_spec.sf,
                 format(Sys.time(), "%Y-%m-%d"),
                 "_target_spec.gpkg"),
          layer = "spec_occ")
+
+
+#  ---------------------------------------
+# Environmental data ---------------------------------------
+#  ---------------------------------------
+library(terra)
+library(sf)
+# Most was borrowed from the CLN CUB project. Here are some additions.
+
+# Use slope as the default raster
+slope.sr <- rast("data/predictors/slope.tif")
+empty.sr <- rast(slope.sr[[1]])
+
+
+# NLCD Impervious surfaces ---------------------------------------
+nlcd_imp_pre <- rast("~/Data/Environment/NLCD/NLCD_2019_ImperviousSurfaces/nlcd_2019_impervious_l48_20210604.img")
+
+nlcd_imp.sr <- nlcd_imp_pre |>
+  project(empty.sr) |>
+  crop(empty.sr) |>
+  app(fun = as.numeric)
+
+names(nlcd_imp.sr) <- "Impervious_surfaces"
+varnames(nlcd_imp.sr) <- "Impervious_surfaces"
+
+# Does it stack?
+# c(nlcd_imp.sr, empty.sr)
+# plot(nlcd_imp)
+
+writeRaster(nlcd_imp.sr, "data/predictors/nlcd_impervious.tif", overwrite = T)
+
+
+# Christine's NLCD Land Cover Class ---------------------------------------
+# Seems to be the same as the original NLCD Land Cover Data
+# nlcd_chr_pre <- rast("~/Data/Environment/NLCD/NLCD_ChristineCustom/nlcd_2019_land_cover_l48_20210604/nlcd_2019_land_cover_l48_20210604.img")
+
+# nlcd_chr2_pre <- rast("~/Data/Environment/NLCD/NLCD_ChristineCustom/nlcd_2001_2019_change_index_l48_20210604/nlcd_2001_2019_change_index_l48_20210604.img")
+
+
+# plot(nlcd_chr_pre)
+
+# nlcd  <- rast("data/predictors/nlcd_landcover.tif")
+# plot(nlcd)
+
+# CES Pollutants and Traffic ---------------------------------------
+ces.sf <- read_sf("~/Data/Environment/CalEnviroScreen4.0/calenviroscreen40gdb_F_2021.gdb/")
+ces.v <- vect("~/Data/Environment/CalEnviroScreen4.0/calenviroscreen40gdb_F_2021.gdb/")
+
+ces_crop.v <-
+  ces.v |>
+  makeValid() |>
+  project(empty.sr) |>
+  crop(empty.sr)
+
+ces_traffic.sr <- ces_crop.v |> 
+  rasterize(empty.sr, "Traffic")
+# plot(ces_traffic.sr)
+
+ces_pollutant.sr <- ces_crop.v |> 
+  rasterize(empty.sr, "PollutionScore")
+# plot(ces_pollutant.sr)
+
+
+# # CIscore data is incomplete. I will fill it in with a quick and dirty 'focal'
+# # TODO
+# newCI.sr <- terra::focal(ces.stack["CIscore"], w = 299, # in cell size, not sure why it has to be an odd number??
+#                          na.policy = "only", fun = "mean",
+#                          na.rm = TRUE)
+
+# names(newCI.sr) <- "CIscore"
+
+# new_ces.stack <- c(newCI.sr, ces.stack["Traffic"])
+
+writeRaster(ces_traffic.sr, "data/predictors/CES_traffic.tif", overwrite = T)
+writeRaster(ces_pollutant.sr, "data/predictors/CES_pollution.tif", overwrite = T)
